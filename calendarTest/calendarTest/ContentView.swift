5adf29d9d8cbeb4f34a2402335f643665b36f979
Salvador Pruneda <93486867+Spruneda134@users.noreply.github.com>
2024-09-04 13:28:52 -0500

    add file

---
+++ calendarTest/calendarTest/ContentView.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            
+            Text("Calendar")
+                .font(.largeTitle)
+                .fontWeight(.medium)
+            
+            buttonview()
+            
+        }
+        }
+    
+
+}
+
+#Preview {
+    ContentView()
+}
---
+++ calendarTest/calendarTest/calendarTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  calendarTestApp.swift
+//  calendarTest
+//
+//  Created by Salvador Pruneda on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct calendarTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
