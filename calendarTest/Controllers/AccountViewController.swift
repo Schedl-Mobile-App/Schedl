9b1398d5aa9d0fd234bbedd568f6666ab89c2d9b
NERO-FAN <139479010+NERO-FAN@users.noreply.github.com>
2024-09-26 00:03:11 -0500

    Added a route for fetching dummy user. Currently, the username is fetched and then displayed in the account page, but no UI has been added as of now. Will be working on creating the rest of the models and routes for the app from here on out.

--- calendarTest/Controllers/AccountViewController.swift
+++ calendarTest/Controllers/AccountViewController.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct AccountViewController: View {
     
-    @State private var data: [String: Any] = [:]
+    @State private var userData: User? = nil
+    @State private var errorMsg: String? = nil
     @State private var isLoading = true
     
     var body: some View {
@@ -17,16 +18,26 @@ struct AccountViewController: View {
                     if isLoading {
                         Text("Loading...")
                             .onAppear {
-                                FirebaseManager.shared.fetchData { fetchedData in
-                                    if let fetchedData = fetchedData {
-                                        self.data = fetchedData
+                                FirebaseManager.shared.fetchUser {
+                                    user, error in
+                                    DispatchQueue.main.async {
+                                        if let user = user {
+                                            self.userData = user
+                                        } else if let error = error {
+                                                self.errorMsg = error.localizedDescription
+                                        }
+                                        self.isLoading = false
                                     }
-                                    self.isLoading = false
                                 }
                             }
                     } else {
                         // Display your data here
-                        Text("Data: \(data.description)")
+                        if errorMsg != nil {
+                            Text(errorMsg ?? "No error message")
+                        }
+                        else {
+                            Text("Account Username: \(userData?.username ?? "Username not Found")")
+                        }
                     }
                 }
                 .padding()
