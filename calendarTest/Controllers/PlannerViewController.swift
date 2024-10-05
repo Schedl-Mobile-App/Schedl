882db4503b3a85ef4b4e466a94746b6af754a340
Salvador Pruneda <salvadorpruneda@Salvadors-MBP.lan>
2024-10-04 19:07:13 -0500

    Added personal event list Tab, everything is hard coded

--- calendarTest/Controllers/PlannerViewController.swift
+++ calendarTest/Controllers/PlannerViewController.swift
@@ -1,18 +1,77 @@
-//
-//  monthview.swift
-//  calendarTest
-//
-//  Created by Salvador Pruneda on 9/2/24.
-//
-
 import SwiftUI
 
+struct Event: Identifiable {
+    let id = UUID()
+    let name: String
+    let host: String
+    let date: String
+    let location: String
+}
+
+
 struct PlannerViewController: View {
+    let events = [
+        Event(name: "Team Meeting", host: "Henry Adams", date: "Oct 10, 2024 at 3:00 PM", location: "Online"),
+        Event(name: "Birthday Party", host: "Son Goku", date: "Oct 12, 2024 at 7:00 PM", location: "987 W. 28th St"),
+        Event(name: "Gym Session", host: "Arnold", date: "Oct 15, 2024 at 10:00 AM", location: "Trufit 10th St")
+    ]
+    
     var body: some View {
-        Text("My Planners View")
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 20) {
+                    ForEach(events) { event in
+                        NavigationLink(destination: EventView()) {
+                            EventCardView(event: event)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle("My Planners View")
+        }
     }
 }
 
+struct EventCardView: View {
+    let event: Event
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(event.name)
+                        .font(.headline)
+                    Text("Hosted by: \(event.host)")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                    Text(event.date)
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                    Text("Location: \(event.location)")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                }
+                Spacer()
+
+                Button(action: {
+                    print("Event '\(event.name)' favorited!")
+                }) {
+                    Image(systemName: "star")
+                        .foregroundColor(.blue)
+                }
+            }
+            .padding()
+            .background(Color.white)
+            .cornerRadius(10)
+            .shadow(radius: 5)
+        }
+        .contentShape(Rectangle())
+    }
+}
+
+// Preview
 #Preview {
     PlannerViewController()
 }
